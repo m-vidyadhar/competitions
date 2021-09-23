7c244a4af19527e55c78c383e03064334d8858d1
m-vidyadhar <m.vidyadhar95@gmail.com>
2021-09-23 18:53:47 +0530

    new project

---
+++ av/age_detection/dataload.py
@@ -0,0 +1,11 @@
+from os import listdir
+
+import numpy as np
+import pandas as pd
+from random import sample
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+from PIL import Image
+
+import tensorflow as tf
